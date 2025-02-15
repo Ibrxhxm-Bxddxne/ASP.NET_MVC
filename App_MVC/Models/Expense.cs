2a199198d9d0fe9f189496fd3f29cf482e8f6341
Ibrahim <brahem?bad662@gmail.com>
2025-02-15 11:02:20 +0100

    Ajoutez des fichiers projet.

---
+++ App_MVC/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App_MVC.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Valeur { get; set; }
+        [Required] 
+        public string? Description { get; set; }
+    }
+}
---
+++ App_MVC/Models/ExpensesDB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App_MVC.Models
+{
+    public class ExpensesDB : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public ExpensesDB(DbContextOptions<ExpensesDB> options) : base(options)
+        {
+            
+        }
+    }
+}
